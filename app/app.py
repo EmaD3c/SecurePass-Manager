82a43f61632138cf1e941472e78d30a4de69bf15
EmaD3c <>
2025-02-24 13:51:13 +0100

    modify docker file and diocker compose to link it with the api

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from app.routes.auth import auth_bp
 def create_app():
     app = Flask(__name__)
 
-    # Enregistre le Blueprint d'authentification
+    # blueprint
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
     return app
