655a68c60f547b952d94129468d555e975d66c83
EmaD3c <>
2025-03-06 17:30:09 +0100

    add request to link backend and frontend

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from routes.auth import auth_bp
 from controllers.database_handler import db
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     # blueprint
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,46 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+async function registerUser(email, password) {
+  const response = await fetch("http://localhost:8000/api/auth/register", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+  });
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  const data = await response.json();
+  console.log(data);
+}
 
-setupCounter(document.querySelector('#counter'))
+async function loginUser(email, password) {
+  const response = await fetch("http://localhost:8000/api/auth/login", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+      console.log("Token reçu :", data.token);
+      localStorage.setItem("token", data.token);
+
+  } else {
+      console.error("Erreur de connexion :", data.error);
+  }
+}
+
+// butons
+
+document.getElementById("register-btn").addEventListener("click", () => {
+  const email = document.getElementById("register-email").value;
+  const password = document.getElementById("register-password").value;
+  registerUser(email, password);
+});
+
+document.getElementById("login-btn").addEventListener("click", () => {
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+  loginUser(email, password);
+});
